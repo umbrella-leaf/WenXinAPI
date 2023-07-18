81d8ba6544a4e1df6aaadfeb3dc53916c493c887
Liu Zhongnuo <2313678365@qq.com>
2023-07-18 23:33:07 +0800

    refactor: 移动测试代码到test目录下

--- src/WenXinAPI/V1.py
+++ src/WenXinAPI/V1.py
@@ -278,19 +278,4 @@ class ChatBot:
         self.conversations[convo_id] = []
 
 
-async def main():
-    chatbot = ChatBot(
-        api_key="OZsbQZGeGKktgPbzhow2KRPZ",
-        secret_key="0PPBxji0T5pZUaF6GX8y6hgP0PvsV74z"
-    )
-    print(await chatbot.ask(prompt="你好！"))
-    print(await chatbot.ask(prompt="你叫什么名字？"))
-    # print(await chatbot.ask(prompt="我很喜欢你"))
-
-
-if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(main())
-
 
